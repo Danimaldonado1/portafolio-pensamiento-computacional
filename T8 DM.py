150d8a45b4c8892496e6dea45beba53fbc834a6b
Danimaldonado1 <158415708+Danimaldonado1@users.noreply.github.com>
2024-04-23 23:01:59 -0600

    Add files via upload

---
+++ T8 DM.py
@@ -0,0 +1,2 @@
+saludo="Hola, mi nombre es Damiela Maldonado"
+print(saludo)
---
+++ T9,FDMC,1244424.py
@@ -0,0 +1,16 @@
+print("Daniela Maldonado, 1244424")
+
+#Captura de datos
+n1=int(input("ingrese un número: "))
+
+# Constantes de conversión
+Kilometros = n1/ 1000
+Millas = n1 / 1.60934
+Pies = n1/ 3.2808
+Pulgadas = Pies * 12 
+
+# Mostrar los resultados
+print("Millas:", Millas)
+print("Kilometros:", Kilometros)
+print("Pies:",Pies)
+print("Pulgadas:", Pulgadas)
